6f828a82e13fd039607baa09abd8592cbb1d0b34
IndraShikhar <shikhar1575@gmail.com>
2024-09-07 16:25:37 +0530

    final update before testing

--- Linked Polynomial/main.cpp
+++ Linked Polynomial/main.cpp
@@ -12,6 +12,8 @@ void displayNode(struct Node*);
 struct Node* mulNode(struct Node*, struct Node*);
 void deleteDuplicate(struct Node*);
 struct Node* addNode(struct Node*, struct Node*);
+void update(struct Node*&);
+void deleteNode(struct Node*&);
 
 struct Node* addNode(struct Node* node1, struct Node* node2) {
     struct Node* temp = nullptr;
@@ -59,6 +61,14 @@ struct Node* mulNode(struct Node* node1, struct Node* node2) {
     return result;
 }
 
+void deleteNode(struct Node*& node) {
+    while (node) {
+        struct Node* temp = node;
+        node = node->next;
+        delete temp;
+    }
+}
+
 void deleteDuplicate(struct Node* node) {
     if (node == nullptr)
         return;
@@ -100,6 +110,22 @@ void insert(struct Node*& node, int c, int p) {
     }
 }
 
+void update(struct Node*& node) {
+    deleteNode(node);
+    char c;
+    do {
+        cout << "Enter the coefficient of the term";
+        int a;
+        cin >> a;
+        cout << "Enter the power of x in the term";
+        int b;
+        cin >> b;
+        cout << "Is there any other term(y/n):";
+        cin >> c;
+        insert(node, a, b);
+    } while (c == 'y' || c == 'Y');
+}
+
 void displayNode(struct Node* head) {
     if (head == nullptr) {
         cout << "The Node list is empty!" << endl;
@@ -119,18 +145,68 @@ void displayNode(struct Node* head) {
     }
 }
 
+void displayMenu() {
+    cout << "What do wanna do??" << endl;
+    cout << "1.Update the polynomial" << endl;
+    cout << "2.Add both the polynomials" << endl;
+    cout << "3.Multiply both the polynomials" << endl;
+    cout << "4.QUIT" << endl;
+}
+
 int main() {
-    struct Node* node = nullptr;
-    // insert(node, 12, 8);
-    // insert(node, 18, 7);
-    // insert(node, 26, 5);
-    insert(node, 25, 3);
-    insert(node, 32, 1);
-
-    displayNode(node);
-
-    struct Node* res = mulNode(node, node);
-    displayNode(res);
+    struct Node* node1 = nullptr, * node2 = nullptr, * result;
+    int choice;
+    do {
+        displayMenu();
+        cout << "Enter your choice:";
+        cin >> choice;
+        switch (choice) {
+        case 1:
+            int c;
+            cout << "Which polynomial you want to update (1 or 2):";
+            cin >> c;
+            switch (c) {
+            case 1:
+                update(node1);
+                cout << "Updated Polynomial 1:" << endl;
+                displayNode(node1);
+                break;
+            case 2:
+                update(node2);
+                cout << "Updated Polynomial 2:" << endl;
+                displayNode(node2);
+            default:
+                cout << "Invalid choice" << endl << "Exiting to Main menu..." << endl;
+                break;
+            }
+            break;
+        case 2:
+            cout << "Polynomial 1:" << endl;
+            displayNode(node1);
+            cout << "Polynomial 2:" << endl;
+            displayNode(node2);
+            result = addNode(node1, node2);
+            cout << "Result:" << endl;
+            displayNode(result);
+            break;
+        case 3:
+            cout << "Polynomial 1:" << endl;
+            displayNode(node1);
+            cout << "Polynomial 2:" << endl;
+            displayNode(node2);
+            result = mulNode(node1, node2);
+            cout << "Result:" << endl;
+            displayNode(result);
+            break;
+        case 4:
+            cout << "Quiting ...." << endl;
+            break;
+        default:
+            cout << "Invalid choice." << "Exiting to Main menu..." << endl;
+            break;
+        }
+        deleteNode(result);
+    } while (choice != 4);
 
     getchar();
     return 0;
